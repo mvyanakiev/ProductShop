6b2a9da15fc3ce5d4331ecee7609994a34fce22b
Milko Yanakiev <myanakiev@hotmail.com>
2019-03-26 17:27:26 +0200

    register post mapping

---
+++ src/main/java/org/softuni/productshop/domain/models/binding/UserRegisterBindingModel.java
@@ -0,0 +1,9 @@
+package org.softuni.productshop.domain.models.binding;
+
+public class UserRegisterBindingModel  {
+
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String email;
+}
